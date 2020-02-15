0d84b8eae2f92990fbf90cc7fd95dc22b6656202
joshua <joshua@dynepic.com>
2020-02-15 11:33:16 -0500

    vapor decode image success

---
+++ CoreImage.framework/CoreImage_framework.h
@@ -0,0 +1,18 @@
+//
+//  CoreImage_framework.h
+//  CoreImage.framework
+//
+//  Created by Joshua Paulsen on 2/15/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreImage_framework.
+FOUNDATION_EXPORT double CoreImage_frameworkVersionNumber;
+
+//! Project version string for CoreImage_framework.
+FOUNDATION_EXPORT const unsigned char CoreImage_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreImage_framework/PublicHeader.h>
+
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,8 +1,22 @@
 import Vapor
+import CoreImage
+
+
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
    router.get("/") { _ in
        return "<3"
    }
+    
+    if #available(OSX 10.13, *) {
+        router.post(ClassificationRequest.self, at: "/classify") { (request, classificationRequest) -> String in
+            let image = classificationRequest.image
+            let ciImage = CIImage(data: image.data)
+            // TODO: Classify contents of the image.
+            return "success"
+        }
+    }
+    
+    
 }
